46142e9f164d856c5a9d90596eb20f651d56ad95
SUDI DAVID <naohdvd1990@gmail.com>
2019-07-08 13:30:21 +0200

    configured test setup

--- src/index.test.js
+++ src/index.test.js
@@ -9,11 +9,13 @@ describe('our first test', () => {
 });
 
 describe('index.html', () => {
- it('should say hello', () => {
+ it('should say hello', (complete) => {
      const index = fs.readFileSync('./src/index.html', 'utf-8');
      jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello Worl")
+      expect(h1.innerHTML).to.equal("Welcome to JS Learning!");
+      complete();
+      window.close();
      });
  });
 });
